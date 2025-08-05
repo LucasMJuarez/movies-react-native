81eb3ee36b494c21ef857b927c0ecf5091ccfb44
lucas juarez <lujuarez@emeal.nttdata.com>
2025-08-05 15:41:23 -0300

    implement now playing use case

--- src/core/use-cases/movies/now-playing.use-case.ts
+++ src/core/use-cases/movies/now-playing.use-case.ts
@@ -1,14 +1,15 @@
 import { HttpAdapter } from "../../../config/adapters/http/http.adapter";
 import { NowPlayingResponse } from "../../../infrastructure/interfaces/movie-db.responses";
+import { MovieMapper } from "../../../infrastructure/mappers/movie.mapper";
 import type { Movie } from "./entitities/movie.entity";
 
 
- export const moviesNowPlayingUseCase = async (fetcher: HttpAdapter): Promise<Movie[]> => {
-  try {
-   const nowPlayingMovies = await fetcher.get<NowPlayingResponse>("/now_playing");
-   return nowPlayingMovies;
-  } catch (error) {
+export const moviesNowPlayingUseCase = async (fetcher: HttpAdapter): Promise<Movie[]> => {
+    try {
+        const nowPlayingMovies = await fetcher.get<NowPlayingResponse>("/now_playing");
+        return nowPlayingMovies.results.map(MovieMapper.fromMovieDBResultToEntity)
+    } catch (error) {
 
-    throw new Error('Error fetching now playing movies');
-  }
- }
+        throw new Error('Error fetching now playing movies');
+    }
+}
--- src/presentation/hooks/useMovies.tsx
+++ src/presentation/hooks/useMovies.tsx
@@ -1,20 +1,23 @@
-import { useEffect, useState } from "react"
-import { Movie } from "../../core/use-cases/movies/entitities/movie.entity";
-import * as UseCases from '../../core/use-cases'
-import { MovieDBFetcher } from "../../config/adapters/http/movieDB.adapter";
+import { useEffect, useState } from 'react';
+import { Movie } from '../../core/use-cases/movies/entitities/movie.entity';
+import * as UseCases from '../../core/use-cases';
+import { MovieDBFetcher } from '../../config/adapters/http/movieDB.adapter';
 export const useMovies = () => {
-    const [loading, setLoading] = useState<boolean>(false);
-    const [movies, setMovies] = useState<Movie[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [nowPlaying, setNowPlaying] = useState<Movie[]>([]);
 
-    useEffect(() =>{
-        initialLoad()
-    },[])
+  useEffect(() => {
+    initialLoad();
+  }, []);
 
-
-    const initialLoad = async () =>
-    {
-       const nowPlayingMovies = await UseCases.moviesNowPlayingUseCase(MovieDBFetcher)
-}
-
-
-}
+  const initialLoad = async () => {
+    const nowPlayingMovies = await UseCases.moviesNowPlayingUseCase(
+      MovieDBFetcher,
+    );
+    console.log(nowPlayingMovies[0]);
+  };
+  return {
+    isLoading,
+    nowPlaying,
+  };
+};
