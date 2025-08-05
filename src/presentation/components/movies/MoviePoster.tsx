603dbe0d8bdfbd96b2a8227363f142862aba7921
lucas juarez <lujuarez@emeal.nttdata.com>
2025-08-05 20:33:34 -0300

    implement scrollviews with movies and navigate to details

--- src/presentation/components/movies/MoviePoster.tsx
+++ src/presentation/components/movies/MoviePoster.tsx
@@ -1,38 +1,60 @@
-import React from 'react'
-import { Image, StyleSheet, View } from 'react-native'
-import { Movie } from '../../../core/use-cases/movies/entities/movie.entity'
-
+import React from 'react';
+import { Image, StyleSheet, View, Pressable } from 'react-native';
+import { Movie } from '../../../core/use-cases/movies/entities/movie.entity';
+import { NavigationProp, useNavigation } from '@react-navigation/native';
+import { RootStackParams } from '../../navigation/Navigation';
 
 interface Props {
-    movie: Movie;
+  movie: Movie;
+  height?: number;
+  width?: number;
 }
-const MoviePoster = ({ movie }: Props) => {
+const MoviePoster = ({ movie, height, width }: Props) => {
+  const navigation = useNavigation<NavigationProp<RootStackParams>>();
   return (
-    <View style={{...styles.imageContainer, height: 400 , width: 300}}>
-      <Image source={{ uri: movie.poster }} style={styles.image} />
-    </View>
-  )
-}
+    <Pressable
+      onPress={() =>
+        navigation.navigate('Details', { movieId: String(movie.id) })
+      }
+      style={({ pressed }) => ({
+        width,
+        height,
+        opacity: pressed ? 0.7 : 1,
+        marginHorizontal: 10,
+        paddingBottom: 20,
+        paddingHorizontal: 10,
+      })}
+    >
+      <View
+        style={{
+          ...styles.imageContainer,
+          height: height || 400,
+          width: width || 300,
+        }}
+      >
+        <Image source={{ uri: movie.poster }} style={styles.image} />
+      </View>
+    </Pressable>
+  );
+};
 
 const styles = StyleSheet.create({
-    image: {
-        flex: 1,
-        borderRadius: 10,
-
+  image: {
+    flex: 1,
+    borderRadius: 10,
+  },
+  imageContainer: {
+    flex: 1,
+    borderRadius: 18,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 10,
     },
-    imageContainer: {
-        flex: 1,
-        borderRadius:18,
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 10,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 7,
-        elevation: 9
-    }
-    })
+    shadowOpacity: 0.25,
+    shadowRadius: 7,
+    elevation: 9,
+  },
+});
 
-
-export default MoviePoster
+export default MoviePoster;
