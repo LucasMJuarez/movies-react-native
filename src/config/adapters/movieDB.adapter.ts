9f12978420f7b43b01abba24e6a5651afa0a7c91
lucas juarez <lujuarez@emeal.nttdata.com>
2025-08-08 13:05:36 -0300

    add dotenv

--- src/config/adapters/movieDB.adapter.ts
+++ src/config/adapters/movieDB.adapter.ts
@@ -1,10 +1,10 @@
 import { AxiosAdapter } from "./http/axios.adapter";
-// import { THE_MOVIE_DB_KEY } from '@env';
+import { THE_MOVIE_DB_KEY } from '@env';
 
 export const MovieDBFetcher = new AxiosAdapter({
     baseUrl: 'https://api.themoviedb.org/3/movie',
     params:{
-        api_key: 'fafcfc846b9989b79861e2064c27b965',
-        language: 'es',
+    api_key: THE_MOVIE_DB_KEY ?? 'no-key',
+    language: 'es',
     }
 })
